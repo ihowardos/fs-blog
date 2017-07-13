21959945294b22d0ecef35c1da29a5f668dd2247
Sergey Antonov <asergey96@gmail.com>
2017-07-13 12:22:52 +0300

    изменен путь к профилю

--- app/controllers/profiles_controller.rb
+++
@@ -1,7 +0,0 @@
-class ProfilesController < ApplicationController
-  expose_decorated :user
-
-  def show
-    user = User.find(params[:id])
-  end
-end
--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,9 @@
 Rails.application.routes.draw do
   resources :blogs
+  resources :profile, only: [:show]
 
   devise_for :users, controllers: { registrations: "users_controllers/registrations" }
 
-  get 'users/:id', to: "profiles#show", as: 'users'
   get 'blogs/user/:user_id', to: 'blogs#index', as: 'user_blogs'
 
   root to: "blogs#index"
