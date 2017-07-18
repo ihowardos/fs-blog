0171c9bc56ec0caac4c9abea821a8c62b6a4e66f
Sergey Antonov <asergey96@gmail.com>
2017-07-18 13:20:11 +0300

    added not found page

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -5,10 +5,16 @@ class ApplicationController < ActionController::Base
 
   rescue_from Pundit::NotAuthorizedError, with: :user_not_authorized
 
+  rescue_from ActiveRecord::RecordNotFound, :with => :render_404
+
   private
 
     def user_not_authorized
       flash[:warning] = "You are not authorized to perform this action."
       redirect_to(request.referrer || root_path)
+    end
+
+    def render_404
+      render :template => "errors/404", :status => 404
     end
 end
